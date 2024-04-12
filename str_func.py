2a5b69beb64410c230fb032b6991591ba4d93e5d
Vadim Glinnikov <vadimzepis@gmail.com>
2024-04-12 14:01:58 +0300

    update str_func.py  add <func main; script if>

--- str_func.py
+++ str_func.py
@@ -6,3 +6,12 @@ def get_upper_line(text: str) -> str:
 def get_title_line(text: str) -> str:
     """Делает заглавными первые буквы каждого слова"""
     return text.title()
+
+
+def main():
+    print(get_upper_line("correct text"))
+    print(get_title_line("correct text"))
+
+
+if __name__ == "__main__":
+    main()
