e12e8dc38c9b21a0fcb8808ce52dead3aa123f36
Vadim Glinnikov <vadimzepis@gmail.com>
2024-04-12 13:31:25 +0300

    update str_func.py  add func get_title_line  update func upper_line

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
-def upper_line(text: str) -> str:
+def get_upper_line(text: str) -> str:
     return text.upper()
+
+
+def get_title_line(text: str) -> str:
+    """Делает заглавными первые буквы каждого слова"""
+    return text.title()
